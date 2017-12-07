f5ebb54863e8655f62667c20b4acdf7db8918cf2
bodqhrohro <bodqhrohro@yandex.ru>
2017-12-08 01:14:25 +0200

    Improve habraparser cross-platformness

--- divanon/parse/parsers/habraparser.sh
+++ divanon/parse/parsers/habraparser.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # usage: habraparser.sh {habrahabr|geektimes} username
-~/.local/bin/scrapy runspider -a url=https://$1.ru/users/$2/comments/ -o data/scrapy_out/$1_$2.xml -t xml parse/parsers/habraparser.py 2>>error.log
+PATH="$PATH:~/.local/bin"
+scrapy runspider -a url=https://$1.ru/users/$2/comments/ -o data/scrapy_out/$1_$2.xml -t xml parse/parsers/habraparser.py 2>>error.log
